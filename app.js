12afae1c7a52fbbd55b1974199bc64edbbc9ef73
Kineolyan <kineolyan@gmail.com>
2014-12-18 21:08:28 +0100

    Initialize with a basic express app.

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var path = require('path');
+
+var app;
+module.exports = app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(request, response) {
+	response.send('Hello fellows\n');
+});
