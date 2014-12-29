3b35f5cf41cc613ab596fed98167de7ca112022c
Kineolyan <kineolyan@gmail.com>
2014-12-30 00:42:17 +0100

    Make the project compile to the end.

--- server/server.js
+++ server/server.js
@@ -3,18 +3,28 @@ import Player from './game/players/players';
 export default class Server {
 	constructor() {
 		this._players = {};
-		this._nextPlayerId = 0;
+		this._nextPlayerId = (function() {
+			var i = 1;
+			return function() {
+				i += 1;
+				return i.toString();
+			};
+		})();
 	}
 
 	get players() {
 		return this._players;
 	}
 
+  /**
+   * Connects a client to the server.
+   * @param  {Socket} socket with client connection
+   */
 	connect(client) {
-		var player = new Player(client, (++this._nextPlayerId).toString());
+		var player = new Player(client, this._nextPlayerId());
 		this.players[client] = player;
 		client.emit('std', { message: 'welcome', name: player.name, id: player.id });
 	}
 
-	disconnect(client) {}
+	disconnect() {}
 }
