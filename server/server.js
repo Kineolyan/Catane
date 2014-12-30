4a31292b7ad06fa43dae0a16cfe945d1cd0c4cb4
Kineolyan <kineolyan@gmail.com>
2014-12-30 01:42:12 +0100

    Add logs and implement a basic disconnect.

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ export default class Server {
 	constructor() {
 		this._players = {};
 		this._nextPlayerId = (function() {
-			var i = 1;
+			var i = 0;
 			return function() {
 				i += 1;
 				return i.toString();
@@ -23,8 +23,14 @@ export default class Server {
 	connect(client) {
 		var player = new Player(client, this._nextPlayerId());
 		this.players[client] = player;
+		console.log(`[Server] ${player.name} is connected`);
 		client.emit('std', { message: 'welcome', name: player.name, id: player.id });
 	}
 
-	disconnect() {}
+	disconnect(client) {
+		var player = this.players[client] || { name: 'Unknown' };
+
+		console.log(`[Server] ${player.name} is disconnected`);
+		delete this._players[client];
+	}
 }
