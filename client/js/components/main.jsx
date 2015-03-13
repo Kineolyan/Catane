91071b6d0b72578b6d3532a43ca9cce2564492f5
Kineolyan <kineolyan@gmail.com>
2015-03-14 00:05:23 +0100

    Update 6to5 to babel.

--- client/js/components/main.jsx
+++ client/js/components/main.jsx
@@ -1,17 +1,17 @@
 'use strict';
 
 /*
-  Entry point of the interface  
+  Entry point of the interface
 */
 
-require("gulp-6to5/node_modules/6to5/register");
+require("babel/register");
 //declare socket first
 
 var Socket = require('./libs/socket');
 var Globals = require('./libs/globals');
 
 var React = require('react');
-var GameReact = require('./parts/GameReact'); 
+var GameReact = require('./parts/GameReact');
 
 Socket.on(Globals.socket.init, function(data) {
     console.log('game start !');
