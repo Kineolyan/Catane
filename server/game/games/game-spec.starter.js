ad88ba64d865d88b5d33c31bd8d4d9f58c7c0f2d
Kineolyan <kineolyan+github@gmail.com>
2015-04-18 20:45:48 +0200

    Restore tests on plays.

--- server/game/games/game-spec.starter.js
+++ server/game/games/game-spec.starter.js
@@ -23,6 +23,23 @@ export function createPlayer(name) {
 	return info;
 }
 
+const PICK_ARGS = [
+		// Outer city ring
+		[ 0, 2, 1, 2 ],
+		[ 2, 1, 2, 0 ],
+		[ 3, -1, 3, -2 ],
+		[ 2, -2, 2, -3 ],
+		[ 1, -3, 0, -2 ],
+		[ -1, -2, -2, -1],
+		[ -2, 0, -3, 1 ],
+		[ -3, 2, -2, 2 ],
+		[ -2, 3, -1, 1 ],
+		// Inner city ring
+		[ 1, 0, 1, -1 ],
+		[ 0, 1, -1, 0 ],
+		[ -1, 1, 0, 1 ]
+];
+
 /**
  * Creates a new game with a given number of players.
  * @param  {Integer} nbPlayers the number of players in the game
@@ -48,6 +65,28 @@ export function createGame(nbPlayers) {
 		players: players,
 		start: function() {
 			players[0].client.receive('game:start', gameId);
+		},
+		/**
+		 * Picks a city and a road for the given player.
+		 * @param player the player acting
+		 * @param cityX the x-coordinate of the city
+		 * @param cityY the y-coordinate of the city
+		 * @param toX the x-coordinate of the road end
+		 * @param toY the y-coordinate of the road end
+		 */
+		pick: function pick(player, cityX, cityY, toX, toY) {
+			player.client.receive('play:pick:colony', { colony: { x: cityX, y: cityY } });
+			player.client.receive('play:pick:path', { path: { from: { x: cityX, y: cityY }, to: { x: toX, y: toY } } });
+			player.client.receive('play:turn:end');
+		},
+		/**
+		 * Picks randomly cities for the players.
+		 */
+		randomPick: function() {
+			var nbOfPicks = this.players.length * 2;
+			for (let i = 0; i < nbOfPicks; i += 1) {
+				this.pick(this.players[i % this.players.length], ...PICK_ARGS[i]);
+			}
 		}
 	};
 }
