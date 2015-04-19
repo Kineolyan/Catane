51c165bdf4c22f0a33b6fcd6ff94b4219a501fbe
Kineolyan <kineolyan+github@gmail.com>
2015-04-19 17:33:59 +0200

    Make city evolve from colony to city.

--- server/elements/geo/city.js
+++ server/elements/geo/city.js
@@ -16,13 +16,33 @@ export default class City {
 		return this._buildable;
 	}
 
+	isCity() {
+		return !this._buildable;
+	}
+
 	get owner() {
 		return this._owner;
 	}
 
 	set owner(owner) {
-		this._owner = owner;
-		return owner;
+		if (this._owner === null) {
+			this._owner = owner;
+			return owner;
+		} else {
+			throw new Error(`City ${this._location} already owned by ${this._owner}`);
+		}
+	}
+
+	/**
+	 * Transforms a colony into a city.
+	 * @throws Error if the operation is not possible
+	 */
+	evolve() {
+		if (this._buildable) {
+			this._buildable = false;
+		} else {
+			throw new Error(`Cannot evolve the city ${this._location} anymore`);
+		}
 	}
 
 }
