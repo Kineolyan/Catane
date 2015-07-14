6dc356b2f1d1fbc01415bfb162a6ed58dac46dd0
Pierrickouw <pierrick.wauquier@gmail.com>
2015-07-14 21:00:04 +0200

    Making listener unique

--- client/js/components/listener/listener.js
+++ client/js/components/listener/listener.js
@@ -1,16 +1,20 @@
 import StartManager from 'client/js/components/listener/startManager';
 import GameManager from 'client/js/components/listener/gameManager';
 
-export default class Listener {
+class Listener {
 
-  constructor(context) {
-
-    this._startManager = new StartManager(context);
-    this._gameManager = new GameManager(context);
+  constructor() {
+    this._startManager = null;
+    this._gameManager = null;
   }
 
-  startListen() {
+  startListen(context) {
+    this._startManager = new StartManager(context);
+    this._gameManager = new GameManager(context);
     this._startManager.startListen();
     this._gameManager.startListen();
   }
 }
+
+
+export default new Listener();
