5929142f8fceedf6e6705772278698fc0a800eea
Kineolyan <kineolyan@gmail.com>
2015-11-29 17:16:45 +0100

    Rework action components to have a base class.

--- client/js/components/parts/GameInterface/actions/BuildColony.react.js
+++ client/js/components/parts/GameInterface/actions/BuildColony.react.js
@@ -1,37 +1,18 @@
-import MoreartyComponent from 'client/js/components/parts/MoreartyComponent.react.js';
+import AActionButton from 'client/js/components/parts/GameInterface/actions/AActionButton.react';
 import React from 'react'; // eslint-disable-line no-unused-vars
 
-import { gameManager } from 'client/js/components/listener/listener.js';
 import { MyBinding } from 'client/js/components/common/players.js';
 import { Board } from 'client/js/components/libs/globals.js';
 import BuildColonyDelegate from 'client/js/components/listener/delegates/buildColony.js';
 
-import Button from 'client/js/components/parts/GameInterface/Elements/Button.react.js';
+export class BuildColony extends AActionButton {
+	constructor() {
+		super(BuildColonyDelegate);
+	}
 
-export class BuildColony extends MoreartyComponent {
-
-  get gameBinding() {
-    return this.getBinding('game');
-  }
-
-  get myBinding() {
-    return this.getBinding('me');
-  }
-
-  render() {
-	  var label = 'Build colony';
-	  var color = BuildColony.theme.normal;
-	  const activated = this.hasEnoughResources();
-    if (activated) {
-		  if (this.isActive()) { color = BuildColony.theme.focus; }
-    } else {
-		  color = BuildColony.theme.disabled;
-    }
-
-	  return <Button label={label} color={color}
-	                 enable={activated} onClick={this.toggleAction.bind(this)}
-		               {...this.props} />;
-  }
+	get label() {
+		return 'Build colony';
+	}
 
   hasEnoughResources() {
     const myBinding = new MyBinding(this.myBinding);
@@ -41,33 +22,10 @@ export class BuildColony extends MoreartyComponent {
       && resources[Board.resourceName.mouton] >= 1
       && resources[Board.resourceName.ble] >= 1;
   }
-
-  isActive() {
-    return this.gameBinding.get('action') === BuildColonyDelegate.ACTION;
-  }
-
-  toggleAction() {
-    if (!this.isActive()) {
-      gameManager().setDelegate(new BuildColonyDelegate(gameManager()));
-    } else {
-      gameManager().setDelegate(null);
-    }
-  }
 }
 
-BuildColony.theme = {
-	normal: '#ff8200',
-	focus: '#ffbc00',
-	disabled: '#9d3700'
-};
-
-BuildColony.defaultProps = {
-	x: 0,
-	y: 0,
-	height: 20,
-	width: 50
-};
-
+BuildColony.theme = AActionButton.defaultTheme;
+BuildColony.defaultProps = AActionButton.defaultProps;
 BuildColony.displayName = 'BuildColony';
 
 export default BuildColony;
--- client/js/components/parts/GameInterface/actions/BuildRoad.react.js
+++ client/js/components/parts/GameInterface/actions/BuildRoad.react.js
@@ -1,36 +1,17 @@
-import MoreartyComponent from 'client/js/components/parts/MoreartyComponent.react.js';
+import AActionButton from 'client/js/components/parts/GameInterface/actions/AActionButton.react';
 import React from 'react'; // eslint-disable-line no-unused-vars
 
-import { gameManager } from 'client/js/components/listener/listener.js';
 import { MyBinding } from 'client/js/components/common/players.js';
 import { Board } from 'client/js/components/libs/globals.js';
 import BuildRoadDelegate from 'client/js/components/listener/delegates/buildRoad.js';
 
-import Button from 'client/js/components/parts/GameInterface/Elements/Button.react.js';
-
-export class BuildRoad extends MoreartyComponent {
-
-	get gameBinding() {
-		return this.getBinding('game');
+export class BuildRoad extends AActionButton {
+	constructor() {
+		super(BuildRoadDelegate);
 	}
 
-	get myBinding() {
-		return this.getBinding('me');
-	}
-
-	render() {
-		var label = 'Build road';
-		var color = BuildRoad.theme.normal;
-		const activated = this.hasEnoughResources();
-		if (activated) {
-			if (this.isActive()) { color = BuildRoad.theme.focus; }
-		} else {
-			color = BuildRoad.theme.disabled;
-		}
-
-		return <Button label={label} color={color}
-		               enable={activated} onClick={this.toggleAction.bind(this)}
-									 {...this.props} />;
+	get label() {
+		return 'Build road';
 	}
 
 	hasEnoughResources() {
@@ -39,33 +20,10 @@ export class BuildRoad extends MoreartyComponent {
 		return resources[Board.resourceName.bois] >= 1
 			&& resources[Board.resourceName.tuile] >= 1;
 	}
-
-	isActive() {
-		return this.gameBinding.get('action') === BuildRoadDelegate.ACTION;
-	}
-
-	toggleAction() {
-		if (!this.isActive()) {
-			gameManager().setDelegate(new BuildRoadDelegate(gameManager()));
-		} else {
-			gameManager().setDelegate(null);
-		}
-	}
 }
 
-BuildRoad.theme = {
-	normal: '#ff8200',
-	focus: '#ffbc00',
-	disabled: '#9d3700'
-};
-
-BuildRoad.defaultProps = {
-	x: 0,
-	y: 0,
-	height: 20,
-	width: 50
-};
-
+BuildRoad.theme = AActionButton.defaultTheme;
+BuildRoad.defaultProps = AActionButton.defaultProps;
 BuildRoad.displayName = 'BuildRoad';
 
 export default BuildRoad;
